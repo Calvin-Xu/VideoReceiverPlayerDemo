7f261c56062b83b317adea194290896582f749a6
Calvin <15521164897@163.com>
2015-09-05 17:55:57 +0800

    Initial Commit

---
+++ VideoReceiverPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoReceiverPlayerDemo
+//
+//  Created by alex  on 9/5/15.
+//  Copyright (c) 2015 cn.edu.scnu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoReceiverPlayerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoReceiverPlayerDemo
+//
+//  Created by alex  on 9/5/15.
+//  Copyright (c) 2015 cn.edu.scnu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
